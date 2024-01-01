bcdd3da0d03e85c2c7847a55b0b8c742b21556c0
Devpoojit Verma <devpoojit10912@gmail.com>
2024-01-01 23:55:33 +0530

    Fixes in remove duplicates

--- RemoveDuplicates/RemDupMain.java
+++ RemoveDuplicates/RemDupMain.java
@@ -2,7 +2,8 @@ package RemoveDuplicates;
 
 public class RemDupMain {
     public static void main(String[] args) {
+        int[] arr = new int[]{1, 1, 2};
         RemDuplicates ob = new RemDuplicates();
-        
+        System.out.println(ob.removeDuplicates(arr));
     }
 }
