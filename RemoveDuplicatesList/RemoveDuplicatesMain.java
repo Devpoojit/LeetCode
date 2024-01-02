c3bb268d5c5ed14c8b955a376e136a508a7e79c7
Devpoojit Verma <devpoojit10912@gmail.com>
2024-01-03 01:11:10 +0530

    Main Added for remove duplicates w list

--- RemoveDuplicatesList/RemoveDuplicatesMain.java
+++ RemoveDuplicatesList/RemoveDuplicatesMain.java
@@ -2,6 +2,31 @@ package RemoveDuplicatesList;
 
 public class RemoveDuplicatesMain {
     public static void main(String[] args) {
-        
+        RemoveDuplicates ob = new RemoveDuplicates();
+
+        ListNode head = new ListNode(1); // First element
+        head.next = new ListNode(1); // Second element
+        head.next.next = new ListNode(2);
+
+        ListNode test1 = ob.deleteDuplicates(head);
+        System.out.println("[1, 1, 2]");
+        while(test1 != null) {
+            System.out.println(test1.val);
+            test1 = test1.next;
+        }
+        System.out.println("--------------------------------");
+        test1 = null;
+
+        ListNode head2 = new ListNode(1); // First element
+        head2.next = new ListNode(1); // Second element
+        head2.next.next = new ListNode(2);
+        head2.next.next.next = new ListNode(3);
+        head2.next.next.next.next = new ListNode(3);
+
+        test1 = ob.deleteDuplicates(head2);
+        while(test1 != null) {
+            System.out.println(test1.val);
+            test1 = test1.next;
+        }
     }
 }
